a447d5588019cb379ac1b6a8919b44998f6290b7
ZertakSapr <sepreanton@gmail.com>
2024-04-28 15:06:13 +0300

    Initial commit

---
+++ 4-2/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace _4_2.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
